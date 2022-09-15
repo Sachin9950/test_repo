00c8d5fca943bd2bc91869a0a24480eda4fb760d
sachin <ssgomtiwal@gmail.com>
2022-09-16 01:37:02 +0530

    Varriable

---
+++ web 22/p2.js
@@ -0,0 +1,12 @@
+//Define a variable called name with your Name as the assigned value
+//Print the value stored in the variable name
+//Change the variable to store your father's name
+//Print the value stored in the variable name
+//Change the variable again to store your mother's name.
+//Print the value stored in the variable 
+var name ="Sachin Sharma"
+console.log(name)
+ var name="Heera lal"
+console.log(name)
+var name="Indra devi"
+console.log(name)
---
+++ web 22/p4.js
@@ -0,0 +1,6 @@
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+//Print the report card of the student (You can make it look nice by using some keyboard symbols )
+let name="sachin sharma"
+let subject="Maths Science English"
+let grades="98 73 93"
+console.log(name+"\n"+subject+"\n"+grades)
